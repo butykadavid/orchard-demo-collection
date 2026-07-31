3aedc1f816ec223cd1208de0eaae6bf0adb38fba
Dávid Butyka <butyka.david@gmail.com>
2026-07-31 10:19:09 +0200

    deleted incompatibe project

--- turing-machine-assistant/src/components/CodeInputRow.js
+++
@@ -1,9 +0,0 @@
-import CodeInputCell from "./CodeInputCell";
-
-export default function CodeInputRow({code, onDigitOneChange, onDigitTwoChange, onDigitThreeChange}) {
-  return <>
-      <CodeInputCell n={code[0]} onChange={onDigitOneChange} />
-      <CodeInputCell n={code[1]} onChange={onDigitTwoChange} />
-      <CodeInputCell n={code[2]} onChange={onDigitThreeChange} />
-  </>
-}
